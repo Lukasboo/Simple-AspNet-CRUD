69ef1e4c0e27ebcac011dfb76f1613c3ac23ab8d
lucas <lukas_boo@hotmail.com>
2017-06-11 12:43:07 -0300

    first commit

---
+++ AspCRUDExample/Models/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AspCRUDExample.Models
+{
+    public class pessoa
+    {
+        [Key]
+        public int Codigo { get; set; }
+        public string nome { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ AspCRUDExample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspCRUDExample.Startup))]
+namespace AspCRUDExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
